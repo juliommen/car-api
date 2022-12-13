fefd8b3ba5f22b27da1e9c70a9de8e5104d0fd79
Júlio Mendonça <juliommen@gmail.com>
2022-12-12 20:21:12 -0400

    created persistance component for categories

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,22 @@
+import { Category } from "../models/Category";
+
+interface CreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+export class CategoriesRepository {
+  private categories: Category[];
+  constructor() {
+    this.categories = [];
+  }
+  create({ name, description }: CreateCategoryDTO) {
+    const category = new Category();
+    Object.assign(category, { name, description, created_at: new Date() });
+    this.categories.push(category);
+  }
+
+  list() {
+    return this.categories;
+  }
+}
