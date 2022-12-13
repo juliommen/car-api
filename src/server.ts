d3d9fb50eb7594defd47f7476cc2c670302f70f3
Júlio Mendonça <juliommen@gmail.com>
2022-12-12 20:23:31 -0400

    little editions

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
 import express from "express";
 
+import { categoryRoutes } from "./routes/category.routes";
+
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  return res.send("oi");
-});
-
-app.post("/courses", (req, res) => {
-  const { body } = req;
-  return res.json(body);
-});
+app.use("/categories", categoryRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
