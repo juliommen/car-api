6c425c3da24c3ca2aa104182fd59b79efc45df14
juliommen <juliommen@gmail.com>
2023-02-03 16:07:20 -0400

    expanding integration test

--- src/modules/cars/useCases/IntegrationTests.spec.ts
+++ src/modules/cars/useCases/IntegrationTests.spec.ts
@@ -2,6 +2,8 @@ import request from "supertest";
 
 import { app } from "../../../app";
 
+let token;
+
 describe("Create Category Controller Integration Test", () => {
   it("should be able to create a user", async () => {
     const response = await request(app).post("/users").send({
@@ -11,6 +13,30 @@ describe("Create Category Controller Integration Test", () => {
       password: "123",
       avatar: "camilaperfil.com",
     });
+
     expect(response.status).toBe(201);
+  });
+
+  it("should be able to authenticate a user", async () => {
+    const response = await request(app).post("/sessions").send({
+      email: "camila@gmail.com",
+      password: "123",
+    });
+
+    token = response.body.token;
+
+    expect(response.body).toHaveProperty("token");
+  });
+
+  it("it should not be able to create a category with non admin user", async () => {
+    const response = await request(app)
+      .post("/categories")
+      .set({ Authorization: `Bearer ${token}` })
+      .send({
+        name: "sedan",
+        description: "sedan",
+      });
+
+    expect(response.status).toBe(400);
   });
 });
