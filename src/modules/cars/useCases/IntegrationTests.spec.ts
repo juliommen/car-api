b2983521e9acfb55f9f333fa25ec9c247e9e0a7f
juliommen <juliommen@gmail.com>
2023-02-03 15:54:51 -0400

    integration test example with supertest

--- src/modules/cars/useCases/IntegrationTests.spec.ts
+++ src/modules/cars/useCases/IntegrationTests.spec.ts
@@ -3,10 +3,14 @@ import request from "supertest";
 import { app } from "../../../app";
 
 describe("Create Category Controller Integration Test", () => {
-  it("should be able to create a category", async () => {
-    const response = await request(app)
-      .post("/categories")
-      .send({ name: "Test", description: "Test" });
+  it("should be able to create a user", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Camila",
+      driverLicense: "987654321",
+      email: "camila@gmail.com",
+      password: "123",
+      avatar: "camilaperfil.com",
+    });
     expect(response.status).toBe(201);
   });
 });
